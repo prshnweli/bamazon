b7e8371f98587a40e3bd6673073c0c58e6789c94
Prashan Welipitiya <prshn.weli@gmail.com>
2017-10-11 15:50:40 -0700

    added sql connection and git ignore

---
+++ bamazonCustomer.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var connect = require('./connect.js')
+
+var connection = mysql.createConnection(connect.input);
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
